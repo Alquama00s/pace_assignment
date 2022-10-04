3ada12febe8f93d7c0c9fbbd49aa860a9272bddf
alquama00s <alquama00s@outlook.com>
2022-10-04 07:35:22 +0530

    init

---
+++ lib/constants/constants.dart
@@ -0,0 +1,3 @@
+abstract class Constants {
+  static const apiKey = "351f924ffdf54020b003399fa1f5fbfb";
+}
---
+++ lib/widgets/news_option.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewsOption extends StatelessWidget {
+  const NewsOption({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
