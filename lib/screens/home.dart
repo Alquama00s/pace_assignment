d316168b035f3d8684ecff06fc7cfa40fa431ad5
alquama00s <alquama00s@outlook.com>
2022-10-04 12:56:48 +0530

    navigation

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:news_app/widgets/news_option.dart';
 
 class Home extends StatelessWidget {
@@ -7,9 +9,21 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: ListView.builder(
-          itemCount: 6, itemBuilder: (context, i) => NewsOption()),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Align(
+          alignment: Alignment.topCenter,
+          child: Text(
+            'H E A D L I N E S',
+            style: TextStyle(fontSize: 29.sp, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+      body: SizedBox(
+        height: double.infinity,
+        child: ListView.builder(
+            itemCount: 6, itemBuilder: (context, i) => NewsOption()),
+      ),
     );
   }
 }
